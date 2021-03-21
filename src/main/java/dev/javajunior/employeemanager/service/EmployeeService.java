17755a64bd723ebc754862c0f6d68dad800a78fc
Gianluca Giavatto <gianluca.giavatto@gmail.com>
2021-03-21 12:25:17 +0100

    added Employee Service

---
+++ src/main/java/dev/javajunior/employeemanager/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package dev.javajunior.employeemanager.service;
+
+import dev.javajunior.employeemanager.model.Employee;
+import dev.javajunior.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
+}
